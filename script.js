45272055f31b2367aae4a61945df71ae48f8ee1b
Andrijasevic Davor <davor.andrijasevic@amag.ch>
2024-08-12 10:33:20 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const progressBars = document.querySelectorAll('.progress');
+    progressBars.forEach(bar => {
+        const progress = bar.getAttribute('data-progress');
+        bar.style.width = progress + '%';
+    });
+});

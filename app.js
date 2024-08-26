745a3456a4eb2200b765d8388c2fb01797fc9db7
Andrijasevic Davor <davor.andrijasevic@amag.ch>
2024-08-26 11:43:28 +0200

    Umstellung auf Node.js

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: 'empfaenger@example.com',
+    subject: 'Test Email',
+    text: 'Hallo! Das ist eine Test-E-Mail.'
+}, (error, info) => {
+    if (error) {
+        console.log('Error sending email:', error);
+    } else {
+        console.log('Email sent:', info.response);
+    }
+});
+
+
+require('dotenv').config();

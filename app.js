418f6757766ce918ca91c3577599766c51ae34b0
Andrijasevic Davor <davor.andrijasevic@amag.ch>
2024-08-26 13:26:00 +0200

    Code fixed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'CV.html'));
 });
 
 app.listen(3000, () => {
